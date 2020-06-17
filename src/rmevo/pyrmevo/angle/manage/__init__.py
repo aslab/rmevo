e358640b42eb1b8960c4a5f05db2eecbc6b626a8
JorgePH <j.povedah@alumnos.upm.es>
2020-06-17 14:49:16 +0200

    Angle eliminated

--- src/rmevo/pyrmevo/angle/manage/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import absolute_import
-
-from .robotmanager import RobotManager
-from .world import WorldManager
--- src/rmevo/pyrmevo/angle/robogen/spec/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-
-from .robot import RobogenTreeGenerator, make_planar
-from .body import BodyGenerator, get_body_spec
-
-__author__ = 'Elte Hupkes'
